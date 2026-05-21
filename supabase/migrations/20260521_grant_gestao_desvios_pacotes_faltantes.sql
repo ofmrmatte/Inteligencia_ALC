2a04f24a9e050691a334c12627b935b0bafe5911
ofmrmatte <matheusfrafou@gmail.com>
2026-05-21 13:44:46 -0300

    Prepara migration de pacotes faltantes

---
+++ supabase/migrations/20260521_grant_gestao_desvios_pacotes_faltantes.sql
@@ -0,0 +1,7 @@
+grant select, insert, update, delete
+on public.gestao_desvios_pacotes_faltantes
+to authenticated;
+
+grant select, insert, update, delete
+on public.gestao_desvios_pacotes_faltantes
+to service_role;
