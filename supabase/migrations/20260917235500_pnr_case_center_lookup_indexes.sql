7cb41fb2306961fceaa08d5bf536893bb3969de0
Matheus Ferreira Folgado <matheusfrafou@gmail.com>
2026-09-17 21:11:31 -0300

    chore: remove noncanonical Case Center migration filename

--- supabase/migrations/20260917235500_pnr_case_center_lookup_indexes.sql
+++
@@ -1,7 +0,0 @@
-create index if not exists pnr_case_center_cases_latest_batch_idx
-  on public.pnr_case_center_cases(latest_batch_id)
-  where latest_batch_id is not null;
-
-create index if not exists pnr_case_events_cached_by_idx
-  on public.pnr_case_events(cached_by)
-  where cached_by is not null;
